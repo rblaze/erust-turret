815b0097e9d35373bc405f2efb1684d9ffb4f33d
Andrey Sverdlichenko <blaze@rusty.zone>
2023-06-04 15:59:55 -0400

    Add command-line tool to create FS images

--- simplefs/src/bin/buildfs/main.rs
+++ simplefs/src/bin/buildfs/main.rs
@@ -1,9 +1,46 @@
+use crate::builder::SimpleFsBuilder;
+
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
+
 mod builder;
 
 #[cfg(test)]
 mod tests;
 
-// Usage: buildfs -o image [files...]
-fn main() {
-    todo!()
+/// Build a filesystem image from a list of files
+#[derive(Parser, Debug)]
+#[command(about)]
+struct Args {
+    /// Image file name
+    #[arg(short)]
+    output: std::path::PathBuf,
+    files: Vec<std::path::PathBuf>,
+    /// Max image size
+    #[arg(short, long, default_value_t = 4*1024*1024)]
+    capacity: usize,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let mut builder: SimpleFsBuilder = SimpleFsBuilder::new(args.capacity);
+    for filename in args.files {
+        println!("Adding file {}", filename.display());
+        let mut f = File::open(filename)?;
+        let mut data = Vec::new();
+        f.read_to_end(&mut data)?;
+        builder.add_file(data);
+    }
+
+    let bytes = builder.finalize()?;
+
+    println!("Writing image to {}, size {} bytes", args.output.display(), bytes.len());
+    let mut image_file = File::create(args.output)?;
+    image_file.write_all(&bytes)?;
+
+    Ok(())
 }
